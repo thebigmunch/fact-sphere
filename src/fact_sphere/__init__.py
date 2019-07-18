9ba18f482e11c9a267f9b138f7e3d9323de3adc2
thebigmunch <mail@thebigmunch.me>
2019-07-18 18:29:08 -0400

    Cleanup

--- src/fact_sphere/__init__.py
+++ src/fact_sphere/__init__.py
@@ -1,28 +1,10 @@
-from .__about__ import (
-	__author__,
-	__author_email__,
-	__copyright__,
-	__license__,
-	__summary__,
-	__title__,
-	__url__,
-	__version__,
-	__version_info__,
-)
+from .__about__ import *
 from .api import *
 from .facts import *
 from .types import *
 
 __all__ = [
-	'__author__',
-	'__author_email__',
-	'__copyright__',
-	'__license__',
-	'__summary__',
-	'__title__',
-	'__url__',
-	'__version__',
-	'__version_info__',
+	*__about__.__all__,
 	*api.__all__,
 	*facts.__all__,
 	*types.__all__,
